b607f88c020e45f19267bb274b19123bd495b522
Nathan Luk <nathan.luk@heykit.com.au>
2022-11-15 09:37:13 +1100

    initial project

---
+++ zero-based-budget/Views/zero_based_budgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  zero_based_budgetApp.swift
+//  zero-based-budget
+//
+//  Created by Nathan Luk on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct zero_based_budgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
